701e77a90bbebf446e2beaf168ccb87f9a9b249c
AlenM666 <llolek278@gmail.com>
2022-04-25 03:21:20 +0200

    made game area

---
+++ Notes.md
@@ -0,0 +1,23 @@
+### Game area
+Making a game area: 
+    -deklare area size
+    -area color (optionl)
+
+
+
+### Snake and food
+making a player (snake):
+    -snake size
+    -snake color
+    -snake design
+
+maing a object(food):
+    -size
+    -color
+    -design
+
+## Snake and food movment
+-movment keys
+-food movment
+
+
--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
 import './App.css';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div id="game-area" className='snake-game-container' style={{left:0, top:0, width: 837, height: 955}}></div>
+
+      {/* pause screen */}
+      <div className='snake-pause-screen'style={{zindex: 10000}}>
+        <div style={{padding: 10}}>
+          "[paused]"
+          <p>Press [space] to unpause.</p>
+        </div>
+      </div>
+
+      {/* playing field */}
+      <div id='playingField' className='snake-playing-field' style={{left: 20, top: 20, width: 780, height: 880}}></div>
+      
+      <div className='snake-panel-componenet' style={{top: 913, left: 30}}>Length: 1</div>
+      <div className='snake-panel-componenet' style={{top: 913, left: 697}}>highscore: 106</div>
+
+
+      {/* Welcome dialog */}
+      <div id='sbWelcome' className='snake-welcome-sing' style={{zindex: 100}}>
+        <div className='inner-snake-dialog'>
+          Javascript Snake Game
+          <p></p>
+          Use the 
+          <strong>Arro keys </strong>
+          on your keyboard to play the game. On Windows, press F11 to play in Full Screen mode.
+          <p></p>
+        </div>
+          <button className='dialog-button'>Play Game</button>
+
+      </div>
+
+
     </div>
   );
 }
